b922e222ee60fe8e6363cfe4bed24747b962f2a4
Bianca Lee <bianca.lee@berkeley.edu>
2020-01-07 10:24:24 -0800

    @fontface

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./index.css";
+import "./fonts/Brandon_blk.otf";
+import "./fonts/GoogleSans-Bold.ttf";
+import "./fonts/GoogleSans-Regular.ttf";
+import "./index.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
